670c9ee2c32812d8062850030f97e50e81e70700
Ben <bjc9001@gmail.com>
2015-10-01 23:05:38 -0500

    added Random string Generation

--- app/src/main/java/com/example/bjc90_000/testapp/MainActivity.java
+++ app/src/main/java/com/example/bjc90_000/testapp/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.bjc90_000.testapp;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-Button testButton;
+Button testButton; TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +23,8 @@ Button testButton;
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               TextView t = (TextView)  findViewById(R.id.textView);
-                t.setText("hello i did it !!!!!");
+              t = (TextView)  findViewById(R.id.textView);
+                t.setText(generateRandomString());
             }
         });
     }
@@ -46,5 +49,33 @@ Button testButton;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String generateRandomString(){
+        final int STRING_LENGTH =10;
+        Random rg = new Random();
+        char[] returnString= {'a','a','s','e','e','e','4','2','3','t'};
+        int i;
+        for(int j=0;j<STRING_LENGTH;j++){
+
+            i=0;
+            while (i<32)
+            {
+                i = rg.nextInt(126);
+            }
+            returnString[j]=(char) i;
+            Log.e("The Random Sting is",returnString.toString());
+        }
+
+
+
+
+
+
+
+
+
+
+        return new String(returnString);
     }
 }
